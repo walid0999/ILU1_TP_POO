e2e62b1483f987fa55347b0f641062abedbb82cb
walid0999 <walid.kadfi-athmani@univ-tlse3.fr>
2024-10-01 09:44:36 +0200

    Tp2 fin a finir

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,19 @@
+package personnages;
+
+
+public enum Equipement {
+	CASQUE("casque"), BOUCLIER("bouclier");
+	private String nom;
+	
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+
+	public String toString() {
+		return nom;
+	}
+	
+	
+
+}
+
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,9 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement equipements[];
+	private int nbEquipement = 0;
+	
 	
 	public Romain(String nom, int force) {
 		this.nom = nom; 
@@ -34,9 +37,28 @@ public class Romain {
 		assert force_debut > force : "La force d'un Romain  doit diminuer apres le coup";
 		}
 	
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2: {
+			System.out.println("Le soldat"+nom+ "est déjà bien protégé!");
+			break;
+		}
+		case 1:{
+			if(equipements[nbEquipement-1] == equipement) {
+				System.out.println("Le soldat"+nom+ "possède déjà un ");
+				break;
+				
+			}
+		}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		Romain romain = new Romain("Asterix", 6);
-		System.out.println(romain.force);
+		
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
 	}
 	
 } 
